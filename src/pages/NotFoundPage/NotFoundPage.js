b94d958383df1ab8d1d9d73b38f0e12c25b630c0
OnionSmit <minhho.technology@gmail.com>
2021-02-07 23:45:58 +0700

    first commit

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row ">
+                    <div className="col-md-9 my-auto">
+                        <h1>404- Lỗi không tìm thấy trang</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NotFoundPage;
---
+++ src/pages/ProductListPage/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>đây là trang quản lý sản phẩm</h1>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
